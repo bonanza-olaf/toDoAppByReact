d78aa2f124a9694baca47bbc2b0b4ebb063f0ba1
bonanza-olaf <48622695+bonanza-olaf@users.noreply.github.com>
2021-06-19 18:45:02 +0900

    [update] InputTodo.jsxにCSSinJSを実装

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#ffb300",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "6px",
+  borderRadius: "5px"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input placeholder="Add New TODO!" value={todoText} onChange={onChange} />
       <button onClick={onClick}>ADD</button>
     </div>
